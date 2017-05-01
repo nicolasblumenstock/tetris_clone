ca0ecde4cf7fbddf6662c3c1e6011e319d72d6b7
Nicolas Blumenstock <nicpblum@gmail.com>
2017-05-01 10:44:42 -0400

    Further Tests in Draw Method

--- game_draw_tests.py
+++ game_draw_tests.py
@@ -23,49 +23,79 @@ class Draw_Test(object):
 		# self.pointlist = pointlist
 		self.pointlist = (((self.x,self.y),((self.x + 75),self.y),((self.x + 75),(self.y + 25)),((self.x + 50),(self.y + 25)),((self.x + 50),(self.y + 50)),((self.x + 25),(self.y + 50)),((self.x + 25),(self.y + 25)),(self.x,(self.y + 25))))
 		self.screen = screen
+		self.move_left = False
+		self.move_right = False
+		self.drop_down = False
+		self.rotate = False
+
+	def buttons(self):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+			elif (event.type == pygame.KEYDOWN):
+				if event.key == pygame.K_LEFT:
+					self.should_move('left', True)
+				elif (event.key == pygame.K_RIGHT):
+					self.should_move('right', True)
+				elif (event.key == pygame.K_DOWN):
+					self.should_move('drop_down', True)
+				elif (event.key == pygame.K_UP):
+					self.should_move('turn_left', True)
+				# elif event.key == 101:
+				# 	self.should_move('turn_right', True)
+				elif event.key == pygame.K_ESCAPE:
+					pygame.quit()
+					quit()
+			elif event.type == pygame.KEYUP:
+				if event.key == pygame.K_LEFT:
+					self.should_move('left', False)
+				if event.key == pygame.K_RIGHT:
+					self.should_move('right', False)
+				if event.key == pygame.K_DOWN:
+					self.should_move('drop_down', False)
+				if event.key == pygame.K_UP:
+					self.should_move('turn_left', False)
+
+	def should_move(self,direction,true_or_false):
+		if direction == 'left':
+			self.move_left = true_or_false
+		elif direction == 'right':
+			self.move_right = true_or_false
+		elif direction == 'drop_down':
+			self.drop_down = true_or_false
+		if direction == 'turn_left':
+			self.rotate = true_or_false
 
 	def draw_block(self):
-		# pygame.draw.polygon(self.screen,self.color,self.pointlist)
-		if (self.y > 500):
+		if (self.move_left and self.x > 0 and self.x < 250):
+			self.x += 25
+		elif (self.move_right and self.x > 0 and self.x < 250):
+			self.x -= 25
+		if (self.y > 0  and self.y < 500):
 			self.y += self.fall
-			pygame.draw.polygon(self.screen,self.color,self.pointlist)
+		if (self.drop_down and self.y > 0 and self.y < 500):
+			self.y += 2 * self.fall
+		pygame.draw.polygon(self.screen,self.color,self.pointlist)
 
-	def draw_update(self):
-		self.y += self.fall
 
 
-pointlist_T = (((0,0),(75,0),(75,25),(50,25),(50,50),(25,50),(25,25),(0,25)))
-point_color = (120,108,245)	
 
+# pointlist_T = (((0,0),(75,0),(75,25),(50,25),(50,50),(25,50),(25,25),(0,25)))
+# point_color = (120,108,245)
 while game_on:
 	frame += 1
 	screen.fill(background_color)
-
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
 	block = Draw_Test(screen)
-	# block.draw_update()
+	block.buttons()
 	block.draw_block()
 
 
 
-		# elif pygame.KEYDOWN:
-		# 	if event.key == K_UP:
-		# 		draw_block.should_move('clockwise',True)
-		# 	elif event.key == K_LEFT:
-		# 		draw_block.should_move('left',True)
-		# 	elif event.key == K_DOWN:
-		# 		draw_block.should_move('down',True)
-		# 	elif event.key == K_RIGHT:
-		# 		draw_block.should_move('right',True)
-		# elif pygame.KEYUP:
-		# 	if event.key == K_UP:
-		# 		draw_block.should_move('clockwise',False)
-		# 	elif event.key == K_LEFT:
-		# 		draw_block.should_move('left',False)
-		# 	elif event.key == K_DOWN:
-		# 		draw_block.should_move('down',False)
-		# 	elif event.key == K_RIGHT:
-				# draw_block.should_move('right',False)
-	pygame.display.flip()
+
+
+
+
+	pygame.display.update()
